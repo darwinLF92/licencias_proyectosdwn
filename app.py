5410d1c0b89b600c403eab34c13ab6ebe706775c
darwinLF92 <145072019+darwinLF92@users.noreply.github.com>
2025-07-17 15:28:57 -0600

    se agrega puerto para produccion

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def db_check():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
