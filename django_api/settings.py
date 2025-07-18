61067aabd23bef4abcd6aebff75a7497cbdd24f4
darwinLF92 <145072019+darwinLF92@users.noreply.github.com>
2025-07-18 08:48:01 -0600

    se agrega conexion de bdd

--- django_api/settings.py
+++ django_api/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='nombre_bd'),
+        'USER': config('DB_USER', default='usuario'),
+        'PASSWORD': config('DB_PASSWORD', default='password'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
