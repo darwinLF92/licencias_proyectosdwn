9e698d69d8d99a72f75b0f2588ff2a1bc074d44f
darwinLF92 <145072019+darwinLF92@users.noreply.github.com>
2025-07-18 11:52:15 -0600

    modificacion de allowed hosts

--- django_api/settings.py
+++ django_api/settings.py
@@ -27,7 +27,9 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
+ALLOWED_HOSTS = [
+    'licenciasproyectosdwn-production.up.railway.app'
+]
 
 
 
